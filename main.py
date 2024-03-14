0e46bbfdafe7a1b1814b119c833aef2cb717c1d0
ShellyDekel <shellydekel2003@gmail.com>
2024-03-14 22:51:32 +0200

    added function excelToJson function to transform excel files to json files

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import pandas
+
+def excelToJson(excelPath: str, jsonDirectoryPath: str, jsonFileName: str):
+    excelFile = pandas.read_csv(excelPath)
+    jsonFile = open(jsonDirectoryPath + "/" + jsonFileName + ".json", "w")
+    jsonFile.write(excelFile.to_json(orient='records'))
+
 def main():
-    print("Hello World!")
+    excelToJson("MadaReports - MadaReports.csv", "./mada_reports", "madareports")
 
 if __name__ == "__main__":
     main()
