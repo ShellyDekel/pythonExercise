7e96fb16fb420e66e5fba6a23db75cfd3909ae8d
ShellyDekel <shellydekel2003@gmail.com>
2024-03-14 23:12:45 +0200

    changed variable names to match file type

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas
 
-def excel_to_json(excel_path: str, json_directory_path: str, json_file_name: str):
-    excelFile = pandas.read_csv(excel_path)
+def csv_to_json(csv_path: str, json_directory_path: str, json_file_name: str):
+    csv_file = pandas.read_csv(csv_path)
     json_file = open(json_directory_path + "/" + json_file_name + ".json", "w")
-    json_file.write(excelFile.to_json(orient='records', indent=2))
+    json_file.write(csv_file.to_json(orient='records', indent=2))
 
 def main():
-    excel_to_json("MadaReports - MadaReports.csv", "./mada_reports", "madareports")
+    csv_to_json("MadaReports - MadaReports.csv", "./mada_reports", "madareports")
 
 if __name__ == "__main__":
     main()
