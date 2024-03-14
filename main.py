c7d7dfdee29681b61868dd31f1c003a5b673273b
ShellyDekel <shellydekel2003@gmail.com>
2024-03-14 23:05:44 +0200

    refactored variable and function names to follow code conventions

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas
 
-def excelToJson(excelPath: str, jsonDirectoryPath: str, jsonFileName: str):
-    excelFile = pandas.read_csv(excelPath)
-    jsonFile = open(jsonDirectoryPath + "/" + jsonFileName + ".json", "w")
-    jsonFile.write(excelFile.to_json(orient='records', indent=2))
+def excel_to_json(excel_path: str, json_directory_path: str, json_file_name: str):
+    excelFile = pandas.read_csv(excel_path)
+    json_file = open(json_directory_path + "/" + json_file_name + ".json", "w")
+    json_file.write(excelFile.to_json(orient='records', indent=2))
 
 def main():
-    excelToJson("MadaReports - MadaReports.csv", "./mada_reports", "madareports")
+    excel_to_json("MadaReports - MadaReports.csv", "./mada_reports", "madareports")
 
 if __name__ == "__main__":
     main()
