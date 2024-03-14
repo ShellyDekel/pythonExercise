c1b2b52c9ce7104b140d3654217522019f7991b4
ShellyDekel <shellydekel2003@gmail.com>
2024-03-15 00:48:41 +0200

    added function to standardize date formats across the file.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pandas
 import os
 import json
+from dateutil import parser
+
+def parse_date(date):
+    try:
+        return parser.parse(date, dayfirst=True).strftime('%d.%m.%Y')
+    except:
+        return date
 
 def csv_to_json(csv_path: str, json_directory: str, json_file_name: str):
     if not os.path.isfile(csv_path):
@@ -13,6 +20,9 @@ def csv_to_json(csv_path: str, json_directory: str, json_file_name: str):
         json_file_name += ".json"
     
     csv_file = pandas.read_csv(csv_path)
+    
+    csv_file = csv_file.map(lambda x: parse_date(x))
+    
     json_file_path = os.path.join(json_directory, json_file_name)
     
     with open(json_file_path, "w") as json_file:
