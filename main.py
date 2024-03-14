79bbc00dc6ecf2a9a829c769a23758eae9196e12
ShellyDekel <shellydekel2003@gmail.com>
2024-03-15 00:49:17 +0200

    refactor

--- main.py
+++ main.py
@@ -3,33 +3,37 @@ import os
 import json
 from dateutil import parser
 
+
 def parse_date(date):
     try:
-        return parser.parse(date, dayfirst=True).strftime('%d.%m.%Y')
+        return parser.parse(date, dayfirst=True).strftime("%d.%m.%Y")
     except:
         return date
+
 
 def csv_to_json(csv_path: str, json_directory: str, json_file_name: str):
     if not os.path.isfile(csv_path):
         raise FileNotFoundError("CSV file not found")
-    
+
     if not os.path.isdir(json_directory):
         os.makedirs(json_directory)
-    
+
     if not json_file_name.endswith(".json"):
         json_file_name += ".json"
-    
+
     csv_file = pandas.read_csv(csv_path)
-    
+
     csv_file = csv_file.map(lambda x: parse_date(x))
-    
+
     json_file_path = os.path.join(json_directory, json_file_name)
-    
+
     with open(json_file_path, "w") as json_file:
-        json.dump(csv_file.to_dict(orient='records'), json_file, indent=2)
+        json.dump(csv_file.to_dict(orient="records"), json_file, indent=2)
+
 
 def main():
     csv_to_json("MadaReports - MadaReports.csv", "./mada_reports", "madareports")
+
 
 if __name__ == "__main__":
     main()
