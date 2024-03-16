25858f8e2f57ef032641607a057a2f19f64132a0
ShellyDekel <shellydekel2003@gmail.com>
2024-03-16 19:07:22 +0200

    added code excecution

--- tests/date_parser_tests.py
+++ tests/date_parser_tests.py
@@ -1,5 +1,10 @@
 import unittest
 from main import parse_date
 
+
 class TestDateParser(unittest.TestCase):
     exit
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/file_convertor_tests.py
+++ tests/file_convertor_tests.py
@@ -1,5 +1,10 @@
 import unittest
 from main import csv_to_json
 
+
 class TestConvertor(unittest.TestCase):
     exit
+
+
+if __name__ == "__main__":
+    unittest.main()
