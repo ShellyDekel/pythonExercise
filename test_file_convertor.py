62c58a712edf869c08e3d852579345bdd7878b09
ShellyDekel <shellydekel2003@gmail.com>
2024-03-17 12:22:45 +0200

    fixed function to create temporary csv file for testing, finished first test

--- test_file_convertor.py
+++ test_file_convertor.py
@@ -2,14 +2,17 @@ import unittest
 import tempfile
 import csv
 import os
+import json
 from main import csv_to_json
 
 
 def create_temp_csv_file(data):
-    with tempfile.NamedTemporaryFile(suffix=".csv", delete=False) as temp_csv_file:
-        with csv.writer(temp_csv_file) as csv_writer:
-            for row in data:
-                csv_writer.writerow(row)
+    with tempfile.NamedTemporaryFile(suffix=".csv", delete=False, mode="w") as temp_csv_file:
+        csv_writer = csv.writer(temp_csv_file)
+
+        for row in data:
+            csv_writer.writerow(row)
+
         temp_file_path = temp_csv_file.name
 
     return temp_file_path
@@ -21,20 +24,29 @@ class TestConvertor(unittest.TestCase):
         data = [
             ["variable1", "variable2", "variable3"],
             ["value1", 1, "1/1/2001"],
-            ["value2", 2, "2,2,2002"],
-            ["value3", 3, "3.3.2003"],
         ]
-        
+
         csv_file_path = create_temp_csv_file(data)
 
         temp_json_directory = tempfile.TemporaryDirectory().name
         temp_json_filename = "test.json"
-        
+
         try:
             csv_to_json(csv_file_path, temp_json_directory, temp_json_filename)
-            
-            with open(os.path.join(temp_json_directory, temp_json_filename)) as json_file:
-                self.assertEqual()
+
+            with open(
+                os.path.join(temp_json_directory, temp_json_filename)
+            ) as json_file:
+                self.assertEqual(
+                    json.load(json_file),
+                    [
+                        {
+                            "variable1": "value1",
+                            "variable2": 1,
+                            "variable3": "01.01.2001",
+                        }
+                    ],
+                )
         finally:
             os.remove(csv_file_path)
 
