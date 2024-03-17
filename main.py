24e862b54a0453e73f8291a4eb3b4ca910066d37
ShellyDekel <shellydekel2003@gmail.com>
2024-03-17 14:54:37 +0200

    refactored function to work with only two variables, and added bit of code to create path to json file if it doesn't exist

--- main.py
+++ main.py
@@ -11,28 +11,33 @@ def parse_date(date):
         return date
 
 
-def csv_to_json(csv_path: str, json_directory: str, json_file_name: str):
+def csv_to_json(csv_path: str, json_file_name: str):
     if not os.path.isfile(csv_path) or not csv_path.endswith(".csv"):
         raise FileNotFoundError("CSV file not found")
 
-    if not os.path.isdir(json_directory):
-        os.makedirs(json_directory)
-
+    # TODO make seperate function
     if not json_file_name.endswith(".json"):
         json_file_name += ".json"
+    
+    path = os.path.split(json_file_name)
+    appendedPath = ""
+    
+    for dir in path:
+        appendedPath = os.path.join(appendedPath, dir)
+        if not os.path.isdir(appendedPath) and not dir.endswith(".json"):
+            os.makedirs(appendedPath)
+    # TODO
 
     csv_file = pandas.read_csv(csv_path)
 
     csv_file = csv_file.map(lambda x: parse_date(x))
 
-    json_file_path = os.path.join(json_directory, json_file_name)
-
-    with open(json_file_path, "w") as json_file:
+    with open(json_file_name, "w") as json_file:
         json.dump(csv_file.to_dict(orient="records"), json_file, indent=2)
 
 
 def main():
-    csv_to_json("MadaReports - MadaReports.csv", "./mada_reports", "madareports")
+    csv_to_json("MadaReports - MadaReports.csv", "mada_reports/madareports")
 
 
 if __name__ == "__main__":
