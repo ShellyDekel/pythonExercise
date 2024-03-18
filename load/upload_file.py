8ace306fc43951c1423c23506a14d4dc8feec6ec
ShellyDekel <shellydekel2003@gmail.com>
2024-03-18 12:08:17 +0200

    made upoad file

---
+++ load/upload_file.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+
+def split_data(data, limit):
+    for i in range(0, len(data), limit):
+        yield data[i : i + limit]
+
+
+def to_json(data, destination, base_filename):
+    json_limit = 50000
+    os.makedirs(destination, exist_ok=True)
+
+    if len(data) > json_limit:
+        splitted_data = list(split_data(data, json_limit))
+        for index, chunk in splitted_data:
+            with open(
+                os.path.join(destination, base_filename + str(index + 1) + ".json"), "w"
+            ) as json_file:
+                json.dump(chunk, json_file, indent=2)
+    else:
+        with open(os.path.join(destination, base_filename + ".json"), "w") as json_file:
+            json.dump(data, json_file, indent=2)
--- main.py
+++ main.py
@@ -1,37 +1,8 @@
-import pandas
-import os
-import json
-from dateutil import parser
-
-
-def parse_date(date):
-    try:
-        return parser.parse(date, dayfirst=True).strftime("%d.%m.%Y")
-    except:
-        return date
-
-
-def csv_to_json(csv_path: str, json_file_name: str):
-
-    # load
-    os.makedirs(os.path.dirname(json_file_name), exist_ok=True)
-
-    try:
-        # extract
-        csv_file = pandas.read_csv(csv_path)
-        # transform
-        csv_file = csv_file.map(lambda x: parse_date(x))
-
-        # load
-        with open(json_file_name, "w") as json_file:
-            json.dump(csv_file.to_dict(orient="records"), json_file, indent=2)
-    except:
-        with open(json_file_name, "w") as json_file:
-            pass
+from file_convertor import csv_to_json_convertor
 
 
 def main():
-    csv_to_json("MadaReports - MadaReports.csv", "mada_reports/madareports")
+    csv_to_json_convertor("MadaReports - MadaReports.csv", "mada_reports")
 
 
 if __name__ == "__main__":
