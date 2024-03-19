272b42607069552d202d11e0e6bfc1e1720aea6f
ShellyDekel <shellydekel2003@gmail.com>
2024-03-19 17:53:51 +0200

    added check if function got empty data

--- load/load_json_file.py
+++ load/load_json_file.py
@@ -7,17 +7,20 @@ def split_data(data, limit):
 
 
 def to_json(data, destination, base_filename, file_limit):
-    os.makedirs(destination, exist_ok=True)
-
-    if len(data) <= file_limit:
-        with open(os.path.join(destination, base_filename + ".json"), "w") as json_file:
-            json.dump(data, json_file, indent=2)
+    if not data:
+        raise ValueError("no data to load")
     else:
-        splitted_data = list(split_data(data, file_limit))
-        index = 1
-        for chunk in splitted_data:
-            with open(
-                os.path.join(destination, base_filename + str(index) + ".json"), "w"
-            ) as json_file:
-                json.dump(chunk, json_file, indent=2)
-            index += 1
+        os.makedirs(destination, exist_ok=True)
+
+        if len(data) <= file_limit:
+            with open(os.path.join(destination, base_filename + ".json"), "w") as json_file:
+                json.dump(data, json_file, indent=2)
+        else:
+            splitted_data = list(split_data(data, file_limit))
+            index = 1
+            for chunk in splitted_data:
+                with open(
+                    os.path.join(destination, base_filename + str(index) + ".json"), "w"
+                ) as json_file:
+                    json.dump(chunk, json_file, indent=2)
+                index += 1
