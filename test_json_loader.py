c48abe60c1c61d6b74277a5d595df55d97baa7f0
ShellyDekel <shellydekel2003@gmail.com>
2024-03-19 17:54:01 +0200

    added tests

--- test_json_loader.py
+++ test_json_loader.py
@@ -1,10 +1,15 @@
 from load.load_json_file import to_json
-import unittest, tempfile, json, os
+import unittest, tempfile, json, os, random, string
 from itertools import repeat
 
 
+def random_word(length):
+    letters = string.ascii_lowercase
+    return "".join(random.choice(letters) for i in range(length))
+
+
 class TestConvertor(unittest.TestCase):
-    def test_upload(self):
+    def test_loadRegularData_loadedSuccessfully(self):
         data = [
             {"variable1": "value1", "variable2": 1, "variable3": "01.01.2001"},
             {"variable1": "asdfg", "variable2": 9, "variable3": "01.12.2020"},
@@ -19,32 +24,46 @@ class TestConvertor(unittest.TestCase):
         with open(os.path.join(temporary_dir, base_filename + ".json")) as json_file:
             self.assertEqual(json.load(json_file), data)
 
-    def test_large_data(self):
-        data = list(repeat({"key1": "value1", "key2": "value2"}, 11))
-        # TODO test more than just amount of files created, test files actually hold the expected values
+    def test_dataPassesFileLimit_loadedMultipleFilesSuccessfully(self):
+        data = list(repeat({"key1": random_word(5), "key2": random_word(10)}, 11))
         temporary_dir = tempfile.TemporaryDirectory().name
         base_filename = "testfile"
-        to_json(data, temporary_dir, base_filename)
+        to_json(data, temporary_dir, base_filename, 10)
+        expected_first = data[0:10]
+        expected_second = data[10:11]
 
-        self.assertEqual(len(os.listdir(temporary_dir)), 2)
+        with open(
+            os.path.join(temporary_dir, base_filename + "1.json")
+        ) as first_json, open(
+            os.path.join(temporary_dir, base_filename + "2.json")
+        ) as second_json:
+            self.assertTrue(
+                len(os.listdir(temporary_dir)) == 2
+                and json.load(first_json) == expected_first
+                and json.load(second_json) == expected_second
+            )
 
-    def test_non_existent_directory(self):
-        data = [
-            {
-                "variable1": "value1",
-                "variable2": 1,
-                "variable3": "01.01.2001",
-            }
-        ]
+    def test_directoryDoesntExist_loadedSuccessfullyInNewlyCreatedDirectory(self):
+        data = [{"variable1": "value1", "variable2": 1, "variable3": "01.01.2001"}]
         temporary_dir = tempfile.TemporaryDirectory().name + "/this/dir/does_not/exist"
         base_filename = "testfile"
         try:
-            to_json(data, temporary_dir, base_filename)
+            to_json(data, temporary_dir, base_filename, 10)
             self.assertTrue(
                 os.path.isfile(os.path.join(temporary_dir, base_filename + ".json"))
             )
         except:
-            self.fail("function failed")
+            self.fail("function failed")        
+            
+    def test_noData_raiseErrorAndDontCreateFile(self):
+        data = []
+        temporary_dir = tempfile.TemporaryDirectory().name
+        base_filename = "testfile"
+        
+        with self.assertRaises(ValueError):
+            to_json(data, temporary_dir, base_filename, 10)
+        self.assertFalse(os.path.isfile(os.path.join(temporary_dir, base_filename + ".json")))
+        
 
 
 if __name__ == "__main__":
