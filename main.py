a5465d7903cc8b7bba9708ec14e37275d5eeea60
ShellyDekel <shellydekel2003@gmail.com>
2024-03-20 14:50:47 +0200

    added logger

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from file_convertor import csv_to_json_convertor
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def main():
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.INFO, datefmt='%d.%m.%Y %I:%M:%S %p')
+    logger.info("started converting files")
     csv_to_json_convertor("MadaReports - MadaReports.csv", "mada_reports")
+    logger.info("converted successfully")
 
 
 if __name__ == "__main__":
