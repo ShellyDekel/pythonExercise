dcc859d6e826beff8db3d439f4548cefa708d19a
ShellyDekel <shellydekel2003@gmail.com>
2024-03-20 16:09:13 +0200

    changed log message

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.INFO, datefmt='%d.%m.%Y %I:%M:%S %p')
     logger.info("started converting files")
     csv_to_json_convertor("MadaReports - MadaReports.csv", "mada_reports")
-    logger.info("converted successfully")
+    logger.info("converted successfully, exiting program")
 
 
 if __name__ == "__main__":
