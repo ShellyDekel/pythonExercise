37f886c7f62a23adb17ece1f3e43f98084ff8a2d
ShellyDekel <shellydekel2003@gmail.com>
2024-03-20 14:59:16 +0200

    added error checks, added logger

--- extract/extract_csv_file.py
+++ extract/extract_csv_file.py
@@ -1,10 +1,23 @@
-import pandas
+import pandas, logging, os
 
+logger = logging.getLogger(__name__)
 
 def extract_csv(filepath):
-    file_data = pandas.read_csv(filepath).to_dict(orient="records")
+    logger.info("extracting csv file: " + filepath)
     
-    if not file_data == []:
-        return file_data
+    if not os.path.isfile(filepath):
+        logger.error("error in finding csv file " + filepath + ": file doesn't exist")
+        raise FileNotFoundError("given file doesn't exit")
     else:
-        raise FileNotFoundError("file does not contain any values")
+        try:
+            file_data = pandas.read_csv(filepath).to_dict(orient="records")
+        except:
+            logger.error("error in finding csv file " + filepath + ": wrong file format")
+            raise FileNotFoundError("wrong file format, function requires csv file")
+        
+        if not file_data == []:
+            logger.info("successfully extracted csv file " + filepath)
+            return file_data
+        else:
+            logger.error("error in extracting csv file " + filepath + ": file does not contain any data")
+            raise FileNotFoundError("file does not contain any data")
