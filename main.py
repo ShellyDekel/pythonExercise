184e21a849d2d47afb56201dcfb9459c6f5bc29f
ShellyDekel <shellydekel2003@gmail.com>
2024-03-20 16:15:16 +0200

    fixed datefmt

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.INFO, datefmt='%d.%m.%Y %I:%M:%S %p')
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s: %(message)s",
+        level=logging.INFO,
+        datefmt="%d.%m.%Y %H:%M:%S",
+    )
     logger.info("started converting files")
     csv_to_json_convertor("MadaReports - MadaReports.csv", "mada_reports")
     logger.info("converted successfully, exiting program")
