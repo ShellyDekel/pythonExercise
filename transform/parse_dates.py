22ca81c91acde488b18a22822379995bcde85a5e
ShellyDekel <shellydekel2003@gmail.com>
2024-03-20 15:15:36 +0200

    added logs, added check if given list of date columns is empty

--- transform/parse_dates.py
+++ transform/parse_dates.py
@@ -1,18 +1,36 @@
 from dateutil import parser
-import copy
+import copy, logging
+
+logger = logging.getLogger(__name__)
 
 
 def parse_date(value):
     try:
+        logger.debug("parsing value: " + value)
+
         return parser.parse(value, dayfirst=True).strftime("%d.%m.%Y")
     except:
+        logger.error("value " + value + " is not a valid date")
+
         raise TypeError("value is not a date")
 
 
 def parse_dates_in_dictionary_list(dictionary_list, date_columns):
-    parsed_list = copy.deepcopy(dictionary_list)
-    for key in date_columns:
-        for dictionary in parsed_list:
-            dictionary[key] = parse_date(dictionary[key])
+    logger.info(
+        "parsing dictionary list according to values: [" + ", ".join(date_columns) + "]"
+    )
 
-    return parsed_list
+    if not date_columns:
+        logger.error("no columns were given")
+
+        raise ValueError("no date columns given")
+    else:
+        parsed_list = copy.deepcopy(dictionary_list)
+
+        for key in date_columns:
+            for dictionary in parsed_list:
+                dictionary[key] = parse_date(dictionary[key])
+
+        logger.info("dictionary list parsed successfully")
+
+        return parsed_list
