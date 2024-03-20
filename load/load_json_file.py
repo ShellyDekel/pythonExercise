e361579a25bbdf07c91dd588b1222478246ca14a
ShellyDekel <shellydekel2003@gmail.com>
2024-03-20 15:58:48 +0200

    added logger, simplified split data function to be a singular line

--- load/load_json_file.py
+++ load/load_json_file.py
@@ -1,9 +1,6 @@
-import os, json
+import os, json, logging
 
-
-def split_data(data, limit):
-    for i in range(0, len(data), limit):
-        yield data[i : i + limit]
+logger = logging.getLogger(__name__)
 
 
 def to_json(data, destination, base_filename, file_limit):
@@ -13,10 +10,12 @@ def to_json(data, destination, base_filename, file_limit):
         os.makedirs(destination, exist_ok=True)
 
         if len(data) <= file_limit:
-            with open(os.path.join(destination, base_filename + ".json"), "w") as json_file:
+            with open(
+                os.path.join(destination, base_filename + ".json"), "w"
+            ) as json_file:
                 json.dump(data, json_file, indent=2)
         else:
-            splitted_data = list(split_data(data, file_limit))
+            splitted_data = [data[i:i + file_limit] for i in range(0, len(data), file_limit)]
             index = 1
             for chunk in splitted_data:
                 with open(
