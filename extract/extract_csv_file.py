d607010aebef73a495783c19e2cc0ef028315a50
ShellyDekel <shellydekel2003@gmail.com>
2024-03-20 15:01:04 +0200

    changed logger messages

--- extract/extract_csv_file.py
+++ extract/extract_csv_file.py
@@ -6,18 +6,18 @@ def extract_csv(filepath):
     logger.info("extracting csv file: " + filepath)
     
     if not os.path.isfile(filepath):
-        logger.error("error in finding csv file " + filepath + ": file doesn't exist")
+        logger.error("csv file " + filepath + " cannot be extracted: file doesn't exist")
         raise FileNotFoundError("given file doesn't exit")
     else:
         try:
             file_data = pandas.read_csv(filepath).to_dict(orient="records")
         except:
-            logger.error("error in finding csv file " + filepath + ": wrong file format")
+            logger.error("csv file " + filepath + " cannot be extracted: wrong file format")
             raise FileNotFoundError("wrong file format, function requires csv file")
         
         if not file_data == []:
             logger.info("successfully extracted csv file " + filepath)
             return file_data
         else:
-            logger.error("error in extracting csv file " + filepath + ": file does not contain any data")
+            logger.error("csv file " + filepath + " cannot be extracted: file does not contain any data")
             raise FileNotFoundError("file does not contain any data")
